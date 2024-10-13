7ba9818094525887b8571951385f02f8364bb608
Antonio Cheong <acheong@duti.dev>
2024-10-13 22:15:24 +0100

    see if wavelength functions are correct (they are)

--- antonio/test_wavelength.py
+++ antonio/test_wavelength.py
@@ -1,1 +1,9 @@
-from main import Edge
+# pyright: reportImplicitRelativeImport=false
+from main import find_available_wavelengths, merge_wavelenghts
+
+occupied = [(1, 20), (30, 35)]
+available = find_available_wavelengths(occupied, 1)
+print(available)
+occupied = [(1, 23), (35, 40)]
+available = merge_wavelenghts(available, find_available_wavelengths(occupied, 6), 6)
+print(available)
