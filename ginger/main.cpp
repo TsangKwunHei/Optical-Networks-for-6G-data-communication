0450d8aab9f74cf12f6121eff6f187c129f497e0
ShadowMr-Ginger <xujianqiao020319@163.com>
2024-10-19 14:26:41 +0100

    Update main.cpp

--- ginger/main.cpp
+++ ginger/main.cpp
@@ -5,55 +5,66 @@
 #include <fstream>
 #include <sstream>
 #include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
 
 using namespace std;
 
-struct service {
-    int s;
-    int d;
-    int S;
-    int L;
-    int R;
-    int V;
+struct Service {
+    int source;
+    int destination;
+    int traversed_number;
+    int left_channel;
+    int right_channel;
+    int service_value;
     vector<int> path;
+    vector<int> passed_edges;
+    vector<int> left_channel_per_edge;
+    vector<int> right_channel_per_edge;
+    vector<bool> hold_resource_edges;
     bool alive = true;
     bool need_to_replan = false;
-    vector<bool> hold_resource_path;
-    vector<vector<int>> allPaths
+    vector<vector<int>> allPaths;
 
 };
 
-struct failure_scenario {
-    int ci;
-    vector<int> edge_ids;
-};
 struct replan_details {
     int server_id = -1;
     int S = 0;
     vector<int> path_and_wavelengths;
 };
 
+struct Node {
+    int id;
+    int switching_times = 0;
+    vector<int> connected_edges;
+};
+
+struct Node_for_dfs {
+    int id;
+    vector<int> edgeIndices;
+    Node_for_dfs(int _id, vector<int> _edgeIndices) : id(_id), edgeIndices(_edgeIndices) {}
+};
+
+struct Edge {
+    int connecting_node[2];
+    bool is_broken = false;
+    float importance = 0;
+    bool wave_length_occupied[40] = { false };
+};
 
 // Environment Data
 int N = 5;
 int M = 6;
 // Read Data Section
 
-vector<int> Pi;
-
 vector<int> path_net;
 vector<vector<bool>> adjacency_list;
 
 
-
-
 int J = 2;
-vector<service> init_Services;
 
-
-// 故障发生次数
-const int T1 = 30;
-const int T2 = 0;
 
 float randf()
 {    
@@ -66,82 +77,171 @@ int randi(int min, int max)
 }
 
 
-void Initialize_Environment() {
-    //现在为方便开发，先设置为读取txt当作输入
-    ifstream infile("input.txt");
-    if (!infile) {
-        cout << "Error opening input file." << endl;
-        return;
-    }
-    int M; // Number of edges
-    infile >> N >> M;
 
-    // Read Pi
-    for (int i = 0; i < N; i++) {
-        int pi;
-        infile >> pi;
-        Pi.push_back(pi);
-    }
+//vector<vector<int>> path_net;
 
-    // Initialize adjacency_list
-    adjacency_list.resize(N, vector<bool>(N, false));
+// 全局初始环境参数
+int node_number;
+int edge_number;
+int service_number;
+vector<Node>init_nodes;
+vector<Edge>init_edges;
+vector<Service> init_services;
 
-    // Read edges and fill adjacency_list
-    vector<pair<int, int>> edges(M + 1); // Edge indices start from 1
-    for (int i = 1; i <= M; i++) {
-        int u, v;
-        infile >> u >> v;
-        u--; v--; 
-        adjacency_list[u][v] = true;
-        adjacency_list[v][u] = true; 
-        edges[i] = make_pair(u, v);
-    }
 
-    // Create path_net
-    for (int i = 1; i <= M; i++) {
-        int u = edges[i].first;
-        int v = edges[i].second;
-        path_net.push_back(u);
-        path_net.push_back(v);
-    }
+// 广度优先搜索 计算边的重要度
+void calcEdgeImportance() {
+    for (int sn = 0; sn < service_number; sn++) {
+        int start = init_services[sn].source;
+        int goal = init_services[sn].destination;
 
-    // services
-    int K;
-    infile >> K;
-    for (int i = 0; i < K; i++) {
-        service temp_service;
-        infile >> temp_service.s >> temp_service.d >> temp_service.S >> temp_service.L >> temp_service.R >> temp_service.V;
-        temp_service.s--;
-        temp_service.d--;
-
-        for (int j = 0; j < temp_service.S; j++) {
-            int edge_num;
-            infile >> edge_num;
-            temp_service.path.push_back(edge_num);
-            temp_service.hold_resource_path.push_back(true);
+        multimap<int, int> graph;
+        map<pair<int, int>, int> edgeIndexMap;
+        for (int i = 0; i < edge_number; i++) {
+            graph.insert({ init_edges[i].connecting_node[0],init_edges[i].connecting_node[1] });
+            graph.insert({ init_edges[i].connecting_node[1],init_edges[i].connecting_node[0] });
+            edgeIndexMap[{init_edges[i].connecting_node[0], init_edges[i].connecting_node[1]}] = i;
+            edgeIndexMap[{init_edges[i].connecting_node[1], init_edges[i].connecting_node[0]}] = i;
         }
-        init_Services.push_back(temp_service);
+
+        queue<Node_for_dfs> q;
+        unordered_set<int> visited;
+        vector<vector<int>> allPaths;
+        q.push(Node_for_dfs(start, { start }));
+        visited.insert(start);
+        bool found = false;
+
+        while (!q.empty()) {
+            int levelSize = q.size();
+            vector<vector<int>> currentLevelPaths;
+
+            for (int i = 0; i < levelSize; ++i) {
+                Node_for_dfs current = q.front();
+                q.pop();
+                if (current.id == goal) {
+                    found = true;
+                    allPaths.push_back(current.edgeIndices);
+                }
+
+                if (!found) {
+                    auto range = graph.equal_range(current.id);
+                    for (auto it = range.first; it != range.second; ++it) {
+                        int neighbor = it->second;
+                        if (visited.find(neighbor) == visited.end()) {
+                            vector<int> newEdgeIndices = current.edgeIndices;
+                            newEdgeIndices.push_back(edgeIndexMap[{current.id, neighbor}]);
+                            q.push(Node_for_dfs(neighbor, newEdgeIndices));
+                        }
+                    }
+                }
+            }
+            if (found) {
+                if (sn == 45) {
+                    int a = 1;
+                }
+                break;
+            }
+        }
+        for (int i = 0; i < allPaths.size(); i++) {
+            for (int j = 0; j < allPaths[i].size(); j++) {
+                init_edges[allPaths[i][j]].importance += float(init_services[sn].right_channel - init_services[sn].left_channel + 1)/allPaths.size();
+            }
+        }
     }
-    infile.close();
 }
 
 
-void detect_need_to_replan(vector<service>* services, failure_scenario scene1) {
+void Initialize_Environment() 
+//读取环境数据 Read environment data 
+{
+    // Read N & M.
+    cin >> node_number >> edge_number;
 
-    vector<bool> fiber_is_broken;
-    for (int i = 0; i < M; i++) {
-        fiber_is_broken.push_back(false);
+    // 预分配内存 pre-allocate size
+    init_nodes.resize(node_number);
+    init_edges.resize(edge_number);
+
+    // 读取信道转换次数 Read Channel switching times
+    for (int i = 0; i < node_number; i++) {
+        init_nodes[i].id = i + 1;
+        cin >> init_nodes[i].switching_times;
     }
-    for (int i = 0; i < scene1.ci; i++) {
-        fiber_is_broken[scene1.edge_ids[i]] = true;
+    // 读取边连结信息 Read edge connections
+    for (int i = 0; i < edge_number; i++) {
+        int node1;
+        int node2;
+        cin >> node1 >> node2;
+        init_edges[i].connecting_node[0] = node1;
+        init_edges[i].connecting_node[1] = node2;
+        init_nodes[node1 - 1].connected_edges.push_back(i);
+        init_nodes[node2 - 1].connected_edges.push_back(i);
     }
 
-    for (vector<service>::iterator iter = (*services).begin(); iter != (*services).end(); iter++) {
+    // 读取服务数量 Read J
+    cin >> service_number;
+
+    // 预分配内存 Pre-allocate Service_list
+    init_services.resize(service_number);
+
+    // 读取服务信息 Read services details
+    for (int i = 0; i < service_number; i++) {
+        int S;
+        cin >> init_services[i].source >> init_services[i].destination >> init_services[i].traversed_number >> init_services[i].left_channel >> init_services[i].right_channel >> init_services[i].service_value;
+        S = init_services[i].traversed_number;
+        init_services[i].passed_edges.resize(S);
+        init_services[i].path.resize(S + 1);
+        init_services[i].left_channel_per_edge.resize(S);
+        init_services[i].right_channel_per_edge.resize(S);
+        init_services[i].hold_resource_edges.resize(S);
+        for (int j = 0; j < S; j++) {
+            cin >> init_services[i].passed_edges[j];
+            init_services[i].left_channel_per_edge[j] = init_services[i].left_channel;
+            init_services[i].right_channel_per_edge[j] = init_services[i].right_channel;
+            init_services[i].hold_resource_edges[j] = true;
+            int edge_number = init_services[i].passed_edges[j];
+            if (j == 0) {
+                init_services[i].path[j] = init_edges[edge_number - 1].connecting_node[0];
+            }
+            init_services[i].path[j + 1] = init_edges[edge_number - 1].connecting_node[1];
+            for (int k = init_services[i].left_channel - 1; k < init_services[i].right_channel; k++) {
+                init_edges[edge_number - 1].wave_length_occupied[k] = true;
+            }
+        }
+    }
+
+    /////////////////////////////////////////评估每条边的重要性 evaluate_edges_importance;
+    calcEdgeImportance();
+}
+
+
+int T1 = 0;
+int T = 0;
+
+void Output_Scenarios() {
+    T1 = 30;
+    cout << T1 << endl;
+    for (int i = 0; i < T1; i++) {
+        int failure_nuber = randi(1, 60);
+        vector<int> randlist;
+        randlist.resize(edge_number);
+        for (int j = 0; j < edge_number; j++) {
+            randlist[j] = j;
+        }
+        random_shuffle(randlist.begin(), randlist.end());
+        for (int j = 0; j < failure_nuber; j++) {
+            cout << randlist[j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void detect_need_to_replan(vector<Service>* services, int e_failed) {
+    for (vector<Service>::iterator iter = (*services).begin(); iter != (*services).end(); iter++) {
         int idx = 0;
         for (vector<int>::iterator iter2 = (*iter).path.begin(); iter2 != (*iter).path.end(); iter2++) {
-            if (fiber_is_broken[*iter2]) {
+            if (e_failed == (*iter2)) {
                 (*iter).need_to_replan = true;
-                (*iter).hold_resource_path[idx] = false;
+                (*iter).hold_resource_edges[idx] = false;
             }
             idx++;
         }
@@ -188,39 +288,39 @@ vector<vector<int>> findAllPaths(int startP, int endP, const vector<vector<bool>
     return allPaths;
 }
 
-void rePlan(vector<service>* services, vector<vector<bool>> adjacency_list) {
+void rePlan(vector<Service>* services, vector<Edge>* edges, vector<Node>* nodes) {
 
 }
 
 
-void available_path_search(vector<service>* services, vector<vector<bool>> adjacency_list, failure_scenario scene1) {
-    for (int i = 0; i < scene1.ci; i++) {
-        int p1 = path_net[2 * i];
-        int p2 = path_net[2 * i + 1];
-        adjacency_list[p1][p2] = false;
-        adjacency_list[p2][p1] = false;
-    }
+void available_path_search(vector<Service>* services, vector<vector<bool>> adjacency_list) {
+    //for (int i = 0; i < scene1.ci; i++) {
+    //    int p1 = path_net[2 * i];
+    //    int p2 = path_net[2 * i + 1];
+    //    adjacency_list[p1][p2] = false;
+    //    adjacency_list[p2][p1] = false;
+    //}
 
-    for (vector<service>::iterator iter = (*services).begin(); iter != (*services).end(); iter++) {
-        int startP = (*iter).s - 1;
-        int endP = (*iter).d - 1;
-        /*
-        * 输入例子
-        int startP = 0, endP = 4;
-        vector<vector<bool>> adjacency_list = {
-            {false, true, false, false, true},  // 0号节点与1号和4号节点相邻
-            {true, false, true, false, false},  // 1号节点与0号和2号节点相邻
-            {false, true, false, true, false},  // 2号节点与1号和3号节点相邻
-            {false, false, true, false, true},  // 3号节点与2号和4号节点相邻
-            {true, false, false, true, false}   // 4号节点与0号和3号节点相邻
-        };
-        */
+    //for (vector<Service>::iterator iter = (*services).begin(); iter != (*services).end(); iter++) {
+    //    int startP = (*iter).source - 1;
+    //    int endP = (*iter).destination - 1;
+    //    /*
+    //    * 输入例子
+    //    int startP = 0, endP = 4;
+    //    vector<vector<bool>> adjacency_list = {
+    //        {false, true, false, false, true},  // 0号节点与1号和4号节点相邻
+    //        {true, false, true, false, false},  // 1号节点与0号和2号节点相邻
+    //        {false, true, false, true, false},  // 2号节点与1号和3号节点相邻
+    //        {false, false, true, false, true},  // 3号节点与2号和4号节点相邻
+    //        {true, false, false, true, false}   // 4号节点与0号和3号节点相邻
+    //    };
+    //    */
 
-        // dfs法获取所有路径
-        (*iter).allPaths = findAllPaths(startP, endP, adjacency_list);
-        // 路径全局优化
-        rePlan(services, adjacency_list);
-    }
+    //    // dfs法获取所有路径
+    //    (*iter).allPaths = findAllPaths(startP, endP, adjacency_list);
+    //    // 路径全局优化
+    //    rePlan(services, adjacency_list);
+    //}
 
 }
 
@@ -228,29 +328,36 @@ int main(){
 
     // 初始化环境 Initialize_Environment
     Initialize_Environment();
-    
+
+    // 输出故障场景 Output_Scenarios
+    Output_Scenarios();
+
+    // 接收故障场景总数
+    cin >> T;
+
     // 开始执行scenarios
-    int T = T1 + T2;
     for (int t = 0; t < T; t++) {
-        vector<service> services(init_Services);
-
-        // 模拟一次损坏
-        failure_scenario scene1;
-        scene1.ci = randi(1, 2);
-        vector<int> randperm;
-        for (int m = 0; m < M; m++) {
-            randperm.push_back(m);
+        
+        // 重置服务状态 reset services
+        vector<Service> services(init_services);
+        vector<Edge> edges(init_edges);
+        vector<Node> nodes(init_nodes);
+        // 读取故障总数 read failure_number
+        int ci;
+        cin >> ci;
+        for (int c = 0; c < ci; c++) {
+            // 读取故障边 read failure_edge
+            int e_failed;
+            cin >> e_failed;
+            
+            // 更新故障发生后边的状态 update_edge_status
+            edges[e_failed - 1].is_broken = true;
+            // 检查哪些服务需要重新规划
+            detect_need_to_replan(&services, e_failed);
+            
+            // 执行重规划
+            rePlan(&services, &edges, &nodes);
         }
-        random_shuffle(randperm.begin(), randperm.end());
-        for (int c = 0; c < scene1.ci; c++) {
-            scene1.edge_ids.push_back(randperm[c]);
-        }
-        // 检查哪些服务需要重新规划
-        detect_need_to_replan(&services,scene1);
-        available_path_search(&services, adjacency_list, scene1);
-
- 
-
     }
 
 
